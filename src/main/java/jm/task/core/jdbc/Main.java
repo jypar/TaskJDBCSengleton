f24dbb58c8f777045eef1e0d1c94c53c039f414e
jypar <cholponova.zhypara@gmail.com>
2021-03-02 22:59:43 +0600

    task 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+        userDao.saveUser("Ivan","Grozniy",(byte)56);
+        userDao.saveUser("Karl","Marks",(byte)43);
+        userDao.saveUser("Victor","Tsoy",(byte)30);
+        userDao.saveUser("Alex","Petrovich",(byte)25);
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
